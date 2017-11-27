6f1ec406494acf9f3619ff70e2744c13b08ffc81
Khayet <jakob.herpel@uni-weimar.de>
2017-11-28 00:44:43 +0100

    Add Image struct and image allocation functions.

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-typedef struct
+typedef struct Complex
 {
   float r;
   float i;
 } Complex;
 
 
-typedef struct
+typedef struct Pixel
 {
   uint8_t r;
   uint8_t g;
@@ -16,12 +17,64 @@ typedef struct
 } Pixel;
 
 
+typedef struct Image
+{
+  Pixel** pxs;
+  int w;
+  int h;
+} Image;
+
+
+Image create_image(int w, int h)
+{
+  /* column-first -> our images are lists of columns */
+  /* TODO: this is performance critical, research. */
+
+  Image img;
+  Pixel** pxdata = malloc(w * sizeof(uint8_t*));
+
+  int i;
+  for (i = 0; i < w; i++) {
+    pxdata[i] = malloc(h * sizeof(Pixel));
+  }
+
+  img.pxs = pxdata;
+  img.w = w;
+  img.h = h;
+
+  return img;
+}
+
+void free_image_memory(Image img)
+{
+  int i;
+  for (i = 0; i < img.w; i++) {
+    free(img.pxs[i]);
+  }
+  free(img.pxs);
+}
+
+void write_ppm(Image img, char* dest)
+{
+  /* open dest file.. */
+  /* write ppm header.. */
+  /* write pixel data.. */
+  /* close file.. */
+
+  img.pxs[0][0].r = 255;
+  img.pxs[0][0].g = 255;
+  img.pxs[0][0].b = 255;
+
+  printf("%i\n", img.pxs[0][0].r);
+}
+
+
 int main()
 {
-  /*Complex c = { 12, 11 };*/
-  Pixel black = { 0, 0, 0 };
+  Image img = create_image(1024, 1024);
 
-  printf("(%i,%i,%i)\n", black.r, black.g, black.b);
+  write_ppm(img, "img/image.ppm");
 
+  free_image_memory(img);
   return 0;
 }
