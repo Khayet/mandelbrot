f38f8f0e62c1a08d877c5b4010dfd3cfb75bc647
Khayet <jakob.herpel@uni-weimar.de>
2017-12-08 23:21:50 +0100

    Start mandelbrot visualization. First step: show the four quadrants of the complex plane.

--- mandelbrot.c
+++ mandelbrot.c
@@ -33,7 +33,7 @@ Image create_image(int w, int h)
   Pixel** pxdata = malloc(w * sizeof(uint8_t*));
 
   int i;
-  for (i = 0; i < w; i++) {
+  for (i = 0; i < w; ++i) {
     pxdata[i] = malloc(h * sizeof(Pixel));
   }
 
@@ -48,7 +48,7 @@ Image create_image(int w, int h)
 void free_image_memory(Image img)
 {
   int i;
-  for (i = 0; i < img.w; i++) {
+  for (i = 0; i < img.w; ++i) {
     free(img.pxs[i]);
   }
   free(img.pxs);
@@ -57,14 +57,14 @@ void free_image_memory(Image img)
 
 void write_ppm(Image img, char* dest)
 {
-  int x=0, y=0;
+  int x, y;
 
   FILE* f = fopen(dest, "w");
   fprintf(f, "P6 %d %d 255 ", img.w, img.h); /* ppm header */
 
   /* row-first to conform to ppm specs. */
-  for (y=0; y < img.h; y++) {
-    for (x=0; x < img.w; x++) {
+  for (y = 0; y < img.h; ++y) {
+    for (x = 0; x < img.w; ++x) {
       fwrite(&img.pxs[x][y], sizeof(Pixel), 1, f);
     }
   }
@@ -73,9 +73,64 @@ void write_ppm(Image img, char* dest)
 }
 
 
+Pixel mandelbrot(Complex c)
+{
+  /*
+   * Receives a complex number (location in complex plane) and returns the
+   * appropriate Pixel.
+   */
+
+  Pixel px;
+  if (c.i >= 0) {
+    px.r = 255, px.g = 0, px.b = 255;
+  }
+  else {
+    px.r = 0, px.g = 0, px.b = 255;
+  }
+
+  if (c.r >= 0) {
+    px.g = 255;
+  }
+  else {
+    px.g = 0;
+  }
+
+  return px;
+}
+
+
+void visualize_mandelbrot(Image img, Complex first, Complex second)
+{
+  /*
+   * Visualizes the detail of the complex plane inbetween the two given
+   * complex numbers. first: upper left corner, second: lower right corner.
+   */
+
+  float width = second.r - first.r;
+  float height = first.i - second.i;
+
+  float sample_size_r = width / (float)img.w;
+  float sample_size_i = height / (float)img.h;
+
+  int x, y;
+  Complex sample = first;
+  for (x = 0; x < img.w; ++x) {
+    for (y = 0; y < img.h; ++y) {
+      img.pxs[x][y] = mandelbrot(sample);
+      sample.i -= sample_size_i;
+    }
+    sample.i = first.i;
+    sample.r += sample_size_r;
+  }
+}
+
+
 int main()
 {
   Image img = create_image(1024, 1024);
+  Complex c1 = { -2, 1 };
+  Complex c2 = { 1, -1 };
+  visualize_mandelbrot(img, c1, c2);
 
   write_ppm(img, "image.ppm");
 
