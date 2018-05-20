0f2c1332aa9fd566f0269d4cdf32a893dc37c18a
Jakob Herpel <jakob.herpel@uni-weimar.de>
2018-05-20 23:21:54 +0200

    Add TODO for argument parsing

--- args.c
+++ args.c
@@ -29,7 +29,7 @@ Args parse_args(int argc, char* argv[])
     }
 
     if (0 == strcmp(argv[i], "--resolution") || 0 == strcmp(argv[i], "-r")) {
-      if (i+2 > argc) {
+      if (i+1 > argc) {
         printf("ERROR: --resolution option requires width and height.\n");
         printf("Sample usage: ./mandelbrot --resolution 900 600\n");
         exit(1);
@@ -40,7 +40,7 @@ Args parse_args(int argc, char* argv[])
 
     /* if --section or -s, parse section */
     if (0 == strcmp(argv[i], "--section") || 0 == strcmp(argv[i], "-s")) {
-      if (i+4 > argc) {
+      if (i+3 > argc) {
         printf("ERROR: --section option requires 4 numbers (floats):\n");
         printf("top_left.r top_left.i bottom_right.r bottom_right.i\n");
         printf("r and i represent the real and imaginary part in the "
@@ -56,19 +56,23 @@ Args parse_args(int argc, char* argv[])
 
     /* if --maxiterations or -m, parse maxiterations */
     if (0 == strcmp(argv[i], "--maxiterations") || 0 == strcmp(argv[i], "-m")) {
-      if (i+1 > argc) {
-        printf("ERROR: --maxiterations option but no number of iterations specified.\n");
+      /* TODO: this is wrong, check correctly if parameter value is correct */
+      if (i > argc) {
+        printf("ERROR: --maxiterations option but no number "
+               "of iterations specified.\n");
         printf("Sample usage: ./mandelbrot --maxiterations 800\n");
       }
+      exit(0);
       args.maxiterations = atoi(argv[i+1]);
     }
 
 
     if (0 == strcmp(argv[i], "--filename") || 0 == strcmp(argv[i], "-f")) {
-      if (i+1 > argc) {
+      if (i > argc) {
         /* TODO: check if file location is writable */
         printf("ERROR: --filename option but no file specified.\n");
         printf("Sample usage: ./mandelbrot --filename image.ppm\n");
+        exit(1);
       }
       args.filename = argv[i+1];
     }
